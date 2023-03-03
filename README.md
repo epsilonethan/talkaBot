4d3e67bc4c40904dfe61be75a290cf274e3fd19d
Ethan <Fischer>
2023-03-03 08:27:27 -0600

    adds new 'chat' slash command which sends request to chatGPT

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ chatGPT enabled Discord bot written in JS
 At root of project create a `.env` file. In this file you will need to include the following variables
 - `BOT_TOKEN` - This would be the token for your bot from the Discord Developer Portal
 - `BOT_CLIENT_ID` - This would be the client id for your bot from the Discord Developer Portal
+- `OPENAI_TOKEN` - This would be the access token from OpenAI
 
 # Current Commands
 - `/ping` - TalkaBot will return `Pong!`
--- register-slash-commands.js
+++ register-slash-commands.js
@@ -1,24 +1,43 @@
-import { REST, Routes } from "discord.js";
+import { REST, Routes } from 'discord.js';
+import * as commands from './commands.js';
 
-export function register_commands(TOKEN, CLIENT_ID) {
-	const commands = [
-		{
-			name: 'ping',
-			description: 'Replies with Pong!',
-		},
-	];
+export async function register_slash_commands(TOKEN, CLIENT_ID) {
+	// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+	let commandsArray = [];
+	for (const command of commands.getCommands()) {
+		commandsArray.push(command.data.toJSON());
+	}
 
+	// Construct and prepare an instance of the REST module
 	const rest = new REST({ version: '10' }).setToken(TOKEN);
 
+	// and deploy your commands!
 	(async () => {
 		try {
-			console.log('Started refreshing application (/) commands.');
+			console.log(`Started refreshing ${commandsArray.length} application (/) commands.`);
 
-			await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
+			// Remove old slash commands before setting new ones
+			await rest.get(Routes.applicationCommands(CLIENT_ID))
+				.then(data => {
+					const promises = [];
+					for (const command of data) {
+						const deleteUrl = `${Routes.applicationCommands(CLIENT_ID)}/${command.id}`;
+						promises.push(rest.delete(deleteUrl));
+					}
+					return Promise.all(promises);
+				});
 
-			console.log('Successfully reloaded application (/) commands.');
+			// The put method is used to fully refresh all commands globally with the current set
+			const data = await rest.put(
+				Routes.applicationCommands(CLIENT_ID),
+				{ body: commandsArray },
+			);
+
+			console.log(`Successfully reloaded ${data.length} application (/) commands.`);
 		} catch (error) {
+			// And of course, make sure you catch and log any errors!
 			console.error(error);
 		}
 	})();
 }
+
